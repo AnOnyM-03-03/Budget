7270619ec7b86d73aada3d5067ea9734cfe76a5c
Tolstov <tolstov.dmitrij42@gmail.com>
2022-10-25 20:19:24 +0400

    creating a card component for counting

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <div id="app">
-    
+    <BudgetList
+    :list="list"
+    />
   </div>
 </template>
 
 <script>
-
+import BudgetList from '@/components/Budget-list'
 export default {
   name: 'App',
   components: {
-  }
+    BudgetList,
+  },
+  data:() => ({
+    list:{
+        1:{
+            type: "INCOME",
+            value: 100,
+            comments: "INCOMING comment",
+            id: 1
+        },
+        2:{
+            type: "OUTCOME",
+            value: -50,
+            comments: "OUTCOMING comment",
+            id: 2
+        },
+    }
+  }),
 }
 </script>
 
